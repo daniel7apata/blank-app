a9261532df538b42aa95437829fbc64fea814675
Daniel Zapata <90068737+daniel7apata@users.noreply.github.com>
2024-11-21 17:22:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,12 @@ import openpyxl
 st.title("Visualizador de XLSX")
 
 # Subida de archivo
-uploaded_file = st.file_uploader("Sube un archivo XLSX", type="xlsx")
+uploaded_file = st.file_uploader("Sube un archivo CSV", type="xlsx")
 
 if uploaded_file is not None:
     try:
         # Leer el archivo especificando la codificación y delimitador
-        #df = pd.read_csv(uploaded_file, encoding="ISO-8859-1")  # Codificación común para evitar errores
-        df_data = pd.read_excel(uploaded_file)
+        df = pd.read_csv(uploaded_file, encoding="ISO-8859-1")  # Codificación común para evitar errores
         
         # Mostrar el DataFrame
         st.write("Vista previa del archivo CSV:")
@@ -20,4 +19,4 @@ if uploaded_file is not None:
     except Exception as e:
         st.error(f"Hubo un error al leer el archivo: {e}")
 else:
-    st.write("Sube un archivo XLSX para empezar.")
+    st.write("Sube un archivo CSV para empezar.")
