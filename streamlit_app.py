67329306eaabb208b1a20a9b6e44c14288d9e819
Daniel Zapata <90068737+daniel7apata@users.noreply.github.com>
2024-11-21 17:32:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,15 +16,15 @@ if uploaded_file is not None:
         # Verificar el tipo de archivo y leerlo apropiadamente
         if uploaded_file.name.endswith(".csv"):
             # Leer archivo CSV
-            df = pd.read_csv(uploaded_file, encoding="ISO-8859-1")
+            df_data = pd.read_csv(uploaded_file, encoding="ISO-8859-1")
             st.write("Vista previa del archivo CSV:")
         elif uploaded_file.name.endswith(".xlsx"):
             # Leer archivo XLSX
-            df = pd.read_excel(uploaded_file, engine="openpyxl")
+            df_data = pd.read_excel(uploaded_file, engine="openpyxl")
             st.write("Vista previa del archivo XLSX:")
         
         # Mostrar el DataFrame
-        st.dataframe(df)
+        st.dataframe(df_data)
 
     except Exception as e:
         st.error(f"Hubo un error al leer el archivo: {e}")
