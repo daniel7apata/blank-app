a52f933442271d8c4310b050cafa100b31eb5758
Daniel Zapata <90068737+daniel7apata@users.noreply.github.com>
2024-11-23 03:55:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,17 +7,18 @@ st.title("Web Scrapping")
 
 url_objetivo = st.text_input("Link")
 st.write("El link ingresado es: " + url_objetivo)
-
-
-respuesta = requests.get(url_objetivo)
-
-if respuesta.status_code == 200:
-  soup = BeautifulSoup(respuesta.text, 'html.parser') #response.content
-
-  elements = soup.find_all()
-
-  for i in elements:
-    result = i.get_text(strip=True)
-    st.write(result)
+if not url_objetivo:
+  st.write("Ingrese link")
 else:
-    st.write(f'Error al acceder a la página: {respuesta.status_code}')
+  respuesta = requests.get(url_objetivo)
+  
+  if respuesta.status_code == 200:
+    soup = BeautifulSoup(respuesta.text, 'html.parser') #response.content
+  
+    elements = soup.find_all()
+  
+    for i in elements:
+      result = i.get_text(strip=True)
+      st.write(result)
+  else:
+      st.write(f'Error al acceder a la página: {respuesta.status_code}')
